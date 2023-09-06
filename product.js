dc75bc9164da30b4cd4eb429d29087f2040ab671
vmuser <vmuser@scale1j2mB7C9AO>
2023-09-06 06:32:07 +0000

    setinterval, settimeout

--- product.js
+++ product.js
@@ -1,15 +1,27 @@
 //declare golabl var
 var productList=[];
+
+setInterval(function(){
+    var date=new Date();
+    document.getElementById('todaydate').innerText=date.toLocaleTimeString();
+},1000)
+
+setTimeout(function(){
+    var date=new Date();
+    document.getElementById('todaytimeout').innerText=date.toLocaleTimeString();
+},5000)
+
 function addProduct(){
     var status=validate();
 
     if(status){
         document.getElementById('msg').innerHTML='<span style="color:green">Product added</span>';
-        showProducts(id,name, type, price);
+        showProducts();
+    }
+    else{
+        document.getElementById('msg').innerHTML='<span style="color:red">Error!!!</span>';
     }
         
-     else
-        document.getElementById('msg').innerHTML='<span style="color:red">Error!!!</span>';
  }
 
  function validate(){
@@ -18,10 +30,13 @@ function addProduct(){
      var pname = document.getElementById('txtpname').value;
      var ptype = document.getElementById('ddltype').value;
      var price = document.getElementById('txtprice').value;
-     if(pid!='' && pname!='' && ptype!='Select' && price!='')
-     flag=true;
-     else
-     flag=false;
+     if(pid!='' && pname!='' && ptype!='Select' && price!=''){
+        flag = true;
+     }else{
+        flag = false;
+     }
+     
+     return flag;
 
  }
  function showProducts(){
@@ -36,10 +51,10 @@ function addProduct(){
      var price = document.getElementById('txtprice').value;
     productList.push({"Id":pid,"pname":pname,"type":ptype, "price":price}); //array ,,,obj are in key val pair
     
-    var tableHeader='<table><thead><tr><th>Id</th><th>Pname</th><th>Ptype</th><th><Price></th></tr></table>';
+    var tableHeader='<table><thead><tr><th>Id</th><th>Pname</th><th>Ptype</th><th><Price></th></thead><tbody>';
     var tableBody='';
     for(let i=0; i<productList.length; i++){
-
-    }
-
+        tableBody = tableBody + '<tr><td>' + productList[i].id +'</td><td>' + productList[i].pname + '</td><td>' + productList[i].ptype + '</td><td>' + productList[i].price +'</tr>';
+     }
+     document.getElementById('productdata').innerHTML = tableHeader +tableBody +'</thead></tbody>';
 }
